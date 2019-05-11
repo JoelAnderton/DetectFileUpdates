d8550b99267258d6e695f2358141970d00818542
Joel Anderton <anderton.joel@gmail.com>
2019-05-11 19:50:19 -0400

    read files create save results to text file

--- FileUpdateDetect.py
+++ FileUpdateDetect.py
@@ -10,13 +10,33 @@
 #   3. Run the program again and compare its current state to the state saved in the list
 #
 ######################################################################################################################
-
+import os
+import datetime
+import time
+import shutil
 
 # find all files and files in subfolders: their names and last modified date/time
 
+folder_path = r'/Users/joelanderton/Desktop/Here'
+file_list = []
+file_dic = {}
 
+for root, dirs, files in os.walk(folder_path):
+    for file in files:
+        print(file)
+        print(os.path.join(root, file))
+        mod_time = time.ctime(os.path.getmtime(os.path.join(root, file)))
+
+        file_dic = {file: mod_time}
+        file_list.append(file_dic)
+        print(file_list)
 
 # save the state of the folder in a text file
+with open('file_info.txt', 'w') as text:
+
+    for file_time in file_list:
+        for k, v in file_time.items():
+            text.writelines(k + v + '\n')
 
 
 # compare the text file to the folder's current state
