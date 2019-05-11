3d275463bd75847312671a8664a89027a2fdc169
Joel Anderton <anderton.joel@gmail.com>
2019-05-11 18:23:30 -0400

    Initial commit

---
+++ FileUpdateDetect.py
@@ -0,0 +1,22 @@
+######################################################################################################################
+# Created by: Joel Anderton
+# Created date: 4/11/2019
+#
+# Purpose: To detect all file updates or additions in a given folder and its subfolders compared to the last time
+#          the program was run
+#
+#   1. Run an initial scan of folder and all its subfolders and files
+#   2. Save that information in a list
+#   3. Run the program again and compare its current state to the state saved in the list
+#
+######################################################################################################################
+
+
+# find all files and files in subfolders: their names and last modified date/time
+
+
+
+# save the state of the folder in a text file
+
+
+# compare the text file to the folder's current state
